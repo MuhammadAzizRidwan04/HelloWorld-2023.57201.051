44ec35bd126d0aa685138bb8d87872ee72a883a7
azizridwan <kangziz356@gmail.com>
2024-03-13 15:15:30 +0700

    first commit

--- src/helloworld_202357201051/HelloWorld_202357201051.java
+++ src/helloworld_202357201051/HelloWorld_202357201051.java
@@ -14,7 +14,7 @@ public class HelloWorld_202357201051 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("HELLO WORLD");
     }
     
 }
